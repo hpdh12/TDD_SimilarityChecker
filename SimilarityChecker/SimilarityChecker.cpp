df2e93fc15e0b03b7d21cfd63ddfa41e79e90166
Dongseong <hpdh12@me.com>
2023-07-06 13:37:06 +0900

    [refactor] move internal func to private

--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -5,45 +5,21 @@ using namespace std;
 class SimilarityChecker
 {
 public:
-	bool isLengthSame(string input1, string input2)
-	{
-		return input1.size() == input2.size();
-	}
-
-	double getLenLonger(string input1, string input2)
-	{
-		if (input1.size() > input2.size())
-		{
-			return input1.size();
-		}
-
-		return input2.size();
-	}
-
-	double getLenShorter(string input1, string input2)
-	{
-		if (input1.size() > input2.size())
-		{
-			return input2.size();
-		}
-		return input1.size();
-	}
 
 	int getLengthPoint(string input1, string input2)
 	{
 		if (isLengthSame(input1, input2))
 			return MAX_POINT_LENGTH;
 
-		if ((input1.size() >= input2.size() * 2) ||
-			(input2.size() >= input1.size() * 2))
-			return 0;
+		if (isDoubleLenGap(input1, input2))
+			return MIN_POINT_LENGTH;
 
-		double lenLonger = getLenLonger(input1, input2);
-		double lenShorter = getLenShorter(input1, input2);
+		int lenShorter = getLenShorter(input1, input2);
+		int lenLonger = getLenLonger(input1, input2);
 
 		int lenDiff = lenLonger - lenShorter;
 
-		int result = (1 - lenDiff / lenShorter) * 60;
+		int result = (int)((1 - (double)lenDiff / lenShorter) * MAX_POINT_LENGTH);
 
 		return result;
 
@@ -51,5 +27,36 @@ public:
 
 	enum MAX_POINT {
 		MAX_POINT_LENGTH = 60,
+		MIN_POINT_LENGTH = 0,
 	};
+private:
+	bool isLengthSame(string input1, string input2)
+	{
+		return input1.size() == input2.size();
+	}
+
+	bool isDoubleLenGap(string input1, string input2)
+	{
+		return (input1.size() >= input2.size() * 2) ||
+			(input2.size() >= input1.size() * 2);
+	}
+
+	int getLenLonger(string input1, string input2)
+	{
+		if (input1.size() > input2.size())
+		{
+			return (int)input1.size();
+		}
+
+		return (int)input2.size();
+	}
+
+	int getLenShorter(string input1, string input2)
+	{
+		if (input1.size() > input2.size())
+		{
+			return (int)input2.size();
+		}
+		return (int)input1.size();
+	}
 };
