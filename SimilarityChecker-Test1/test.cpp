d727c2ade545d84765c040f6ae84c93affce34e3
Dongseong <hpdh12@me.com>
2023-07-06 14:15:52 +0900

    [feature] impl get zero-point of alphabet

--- SimilarityChecker-Test1/test.cpp
+++ SimilarityChecker-Test1/test.cpp
@@ -12,3 +12,9 @@ TEST_F(SimilarityCheckerFixture, getAlphaPointMax)
 	int point = checker.getAlphaPoint("ASD", "DSA");
 	EXPECT_EQ(SimilarityChecker::MAX_POINT_ALPHA, point);
 }
+
+TEST_F(SimilarityCheckerFixture, getAlphaPointZero)
+{
+	int point = checker.getAlphaPoint("A", "BB");
+	EXPECT_EQ(0, point);
+}
--- SimilarityChecker/SimilarityChecker.cpp
+++ SimilarityChecker/SimilarityChecker.cpp
@@ -1,4 +1,5 @@
 #include <string>
+#include <map>
 
 using namespace std;
 
@@ -7,6 +8,30 @@ class SimilarityChecker
 public:
 	int getAlphaPoint(string str, string text)
 	{
+		map<char, bool> mapInput1, mapTotal;
+		for (auto ch : str)
+		{
+			mapInput1[ch] = true;
+			mapTotal[ch] = true;
+		}
+		int cntSame = 0;
+		map<char, bool> mapInput2;
+		for (auto ch : text)
+		{
+			if(mapInput2.find(ch) == mapInput2.end())
+			{
+				if (mapInput1.find(ch) != mapInput1.end())
+				{
+					cntSame++;
+				}
+			}
+			mapInput2[ch] = true;
+			mapTotal[ch] = true;
+		}
+
+		if (cntSame == 0)
+			return 0;
+
 		return MAX_POINT_ALPHA;
 	}
 
